56aa210dd32ec93f6499196a675eb3ca21f097db
amosmuthoni288-hue <amosmuthoni288@gmail.com>
2025-11-12 15:00:52 +0300

    Add files via upload

---
+++ numbers_100_to_50_in_a_for_loop.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+    int i;
+    // For loop to display from 100 to 50
+    for (i = 100; i >= 50; i--) {
+        printf("%d \n", i);
+    }
+    return 0;
+}
+    
